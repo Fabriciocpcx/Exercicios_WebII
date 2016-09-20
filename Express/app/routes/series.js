93fb6873dfddfb8761f469eefa84bf1d8fa011f6
Fabricio <fabricio_nathancpcx@hotmail.com>
2016-09-19 22:57:49 -0300

    Express Finalizado!

--- Express/app/routes/series.js
+++ Express/app/routes/series.js
@@ -1,16 +1,16 @@
 module.exports = function (app) {
     var controller = app.controllers.serie;
-    
+
 
     //organizacao das rotas
     app.route('/api/series')
         .get(controller.listaSeries);
 
-    app.route('/api/series/:id')
-        .get(controller.listaSeriesId);
 
     app.route('/api/series/:')
         .get(controller.listaNomeSerie);
 
-        
+    app.route('/api/series/:id')
+        .get(controller.obtemSerie);
+
 }
